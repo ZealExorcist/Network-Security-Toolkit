97e50f3017aeaf17bbac80f8a53421e8c5629711
ZealExorcist <141896261+ZealExorcist@users.noreply.github.com>
2024-04-07 18:22:49 +0530

    files added

---
+++ packet_sniff.py
@@ -0,0 +1,11 @@
+from scapy.all import *
+
+def packet(count):
+    pack = ""
+    packets = sniff(count = count)
+    for packet in packets:
+        pack += str(packet) + "\n"
+    return pack
+
+if __name__ == "__main__":
+    print(packet(5))
