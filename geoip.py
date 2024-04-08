1cdc18f474896ccffeee2fb0d1e8a0c5511fd452
ZealExorcist <rohan.nayakanti@gmail.com>
2024-04-08 13:25:50 +0530

    modified:   geoip.py 	modified:   packet_sniff.py

--- geoip.py
+++ geoip.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import geocoder
 import folium
 import socket
@@ -19,26 +18,6 @@ def get_loc(url, path, file):
 
 if __name__ == "__main__":
     url = input("Enter a url: ")
-=======
-import geocoder
-import folium
-import socket
-
-def get_loc(url, path, file):
-    ip = socket.gethostbyname(url)
-    
-    g = geocoder.ip(ip)
-    myaddress = g.latlng
-
-    myMap = folium.Map(location=myaddress, zoom_start=12)
-    folium.Marker(myaddress, popup="My Location").add_to(myMap)
-    folium.CircleMarker(myaddress, radius=50, color='red', fill_color='red').add_to(myMap)
-
-    myMap.save(path+file+".html")
-    return f'Latitude: {myaddress[0]} Longitude: {myaddress[1]}\nLocation saved to {path+file}.html'
-
-
-if __name__ == "__main__":
-    url = input("Enter a url: ")
->>>>>>> 97e50f3017aeaf17bbac80f8a53421e8c5629711
-    print(get_loc(url, "", 'test'))
+    path = input("Enter a path: ")
+    file = input("Enter a file name: ")
+    print(get_loc(url, path, file))
--- packet_sniff.py
+++ packet_sniff.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from scapy.all import *
 
 def packet(count):
@@ -9,16 +8,4 @@ def packet(count):
     return pack
 
 if __name__ == "__main__":
-=======
-from scapy.all import *
-
-def packet(count):
-    pack = ""
-    packets = sniff(count = count)
-    for packet in packets:
-        pack += str(packet) + "\n"
-    return pack
-
-if __name__ == "__main__":
->>>>>>> 97e50f3017aeaf17bbac80f8a53421e8c5629711
     print(packet(5))
